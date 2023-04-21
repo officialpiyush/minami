07970e945bff728cd08d74a57ac2c9c6c419113b
Piyush <bhangalepiyush@gmail.com>
2023-04-21 22:22:32 +0530

    feat: add horizontal padding to root element

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <body className="min-h-screen">
+      <body className="px-2 min-h-screen">
         <Navbar />
         {children}
       </body>
