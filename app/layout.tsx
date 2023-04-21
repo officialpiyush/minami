04c44b1820fda4c88049ffe67c9f04dd215403e2
Piyush <bhangalepiyush@gmail.com>
2023-04-21 22:55:22 +0530

    feat: add text content on landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <body className="px-2 min-h-screen">
-        <Navbar />
-        {children}
+      <body className="flex flex-col px-2 min-h-screen">
+        <div className="flex-shrink-0">
+          <Navbar />
+        </div>
+        <div className="flex flex-col w-full flex-1 max-w-5xl mx-auto">{children}</div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-import Image from 'next/image'
-
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main>
-        Hello x2
-    </main>
-  )
+    <div className="flex flex-1 items-center">
+      <div className="w-full grid grid-cols-12 gap-4">
+        <div className="col-span-8 space-y-4">
+          <div className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Your mind matters
+          </div>
+
+          <div className="scroll-m-20 text-2xl font-semibold tracking-tight">
+            some inspirational text as to why you should help yourself with
+            Minami and how this is beneficial with a statistical fact
+          </div>
+
+          <div>
+            <Button>
+              Sign In
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
