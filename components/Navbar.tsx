cbb15a6f1a53ac6ae17ce4ca6b55bef2cc3d3e88
Piyush <bhangalepiyush@gmail.com>
2023-04-22 10:55:23 +0530

    feat: auth in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,27 @@
+import { currentUser } from "@clerk/nextjs/app-beta";
 import Logo from "./icons/Logo";
-import { Avatar, AvatarFallback } from "./ui/avatar";
-import { Button } from "./ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
-export default function Navbar() {
-    return (
-        <div className="flex items-center justify-between rounded-full border-2 my-4 max-w-5xl mx-auto px-2 py-2">
-            <div className="bg-[#c9b9ac] rounded-full ">
-                <Logo className="h-14 w-14 p-1" />
-            </div>
+export default async function Navbar() {
+  const user = await currentUser();
 
-            <div className="scroll-m-20 text-2xl font-semibold">
-                minami
-            </div>
+  return (
+    <div className="flex items-center justify-between rounded-full border-2 my-4 max-w-5xl mx-auto px-2 py-2">
+      <div className="bg-[#c9b9ac] rounded-full ">
+        <Logo className="h-14 w-14 p-1" />
+      </div>
 
-            <div>
-                <Avatar className="h-14 w-14">
-                    <AvatarFallback>
-                        <span className="text-2xl">👻</span>
-                    </AvatarFallback>
-                </Avatar>
-            </div>
-        </div>
-    )
+      <div className="scroll-m-20 text-2xl font-semibold">minami</div>
+
+      <div>
+        <Avatar className="h-14 w-14">
+          <AvatarFallback>
+            <span className="text-2xl">👻</span>
+          </AvatarFallback>
+
+          <AvatarImage src={(user && user.profileImageUrl) || undefined} />
+        </Avatar>
+      </div>
+    </div>
+  );
 }
