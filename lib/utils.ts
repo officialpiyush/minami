da3bd552d9c247270e57093dd87feae1def6b91b
Piyush <bhangalepiyush@gmail.com>
2023-04-22 19:03:43 +0530

    fix: change prisma initialization

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,6 @@
-import { PrismaClient } from "@prisma/client";
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const prisma = new PrismaClient();
