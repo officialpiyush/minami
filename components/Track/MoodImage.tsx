61de1973158b9e17648c239f850dfab876f5fd5e
Piyush <bhangalepiyush@gmail.com>
2023-04-22 17:27:34 +0530

    feat: add mood names

--- components/Track/MoodImage.tsx
+++ components/Track/MoodImage.tsx
@@ -6,13 +6,17 @@ interface MoodImageProps extends React.ComponentProps<"div"> {
 export default function MoodImage(props: MoodImageProps) {
   return (
     <div className="p-4 m-4 relative">
-      <div className="rounded-full">
+      <div className="flex flex-col items-center">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           className="z-10 ring-[12px] ring-[#969A8A] h-48 w-48 rounded-full"
           src={props.image}
           alt={props.mood}
         />
+
+        <span className="text-xl bg-[#897C74] mt-6 px-4 py-1 text-white rounded-xl first-letter:capitalize font-fraunces">
+          {props.mood}
+        </span>
       </div>
 
       <div className="-z-10 bg-[#969A8A] rounded-full h-24 w-24 absolute top-0 left-0 -ml-4 -mt-4 flex items-center justify-center">
