af0da8fe3977aebc5595bb6ce7ac1db54613bd35
Piyush <bhangalepiyush@gmail.com>
2023-04-22 18:09:19 +0530

    feat: neditate page

---
+++ app/meditate/page.tsx
@@ -0,0 +1,10 @@
+import PulseIcon from "@/components/icons/PulseIcon";
+import { Ping } from "@uiball/loaders";
+
+export default function MeditatePage() {
+  return (
+    <div className="h-full flex flex-1 flex-col items-center justify-center">
+      <PulseIcon />
+    </div>
+  );
+}
