be28f1ca5a2c284258908b453a87a3451ad74159
Piyush <bhangalepiyush@gmail.com>
2023-04-22 18:56:48 +0530

    feat: initialize and export prisma

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
-import { ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { PrismaClient } from "@prisma/client";
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const prisma = new PrismaClient();
