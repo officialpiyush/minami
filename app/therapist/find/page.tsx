3c4ca16d68ab37161a81229e46a73686e791ff6a
Piyush <bhangalepiyush@gmail.com>
2023-04-23 03:31:17 +0530

    feat: add register button

--- app/therapist/find/page.tsx
+++ app/therapist/find/page.tsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { faker } from "@faker-js/faker";
+import { UserPlus } from "lucide-react";
 import Link from "next/link";
 
 export default function TherapistFindPage() {
@@ -12,9 +13,17 @@ export default function TherapistFindPage() {
 
   return (
     <div className="flex flex-1 flex-col gap-4 pb-4 font-fraunces">
-      <h1 className="py-4 text-center scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Therapists
-      </h1>
+      <div className="flex justify-between items-center">
+        <h1 className="py-4 text-center scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Therapists
+        </h1>
+
+        <div>
+          <button className="flex gap-2 bg-[#D9D9D9] py-2 px-4 rounded-md border">
+            <UserPlus /> Register
+          </button>
+        </div>
+      </div>
       <div className="grid grid-cols-2 gap-4">
         {therapists.map((therapist, index) => (
           <div
@@ -29,7 +38,9 @@ export default function TherapistFindPage() {
             </div>
 
             <div className="flex justify-between items-center px-2">
-              <div className="text-sm text-opacity-30">{therapist.location}</div>
+              <div className="text-sm text-opacity-30">
+                {therapist.location}
+              </div>
               <Link
                 href={`tel:${therapist.contact}`}
                 target="_blank"
