64c4873bff73eb035e4bbcdc15953a69487becc3
Piyush <bhangalepiyush@gmail.com>
2023-04-22 11:12:57 +0530

    feat: link to home page on logo click

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,18 @@
 import { currentUser } from "@clerk/nextjs/app-beta";
 import Logo from "./icons/Logo";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 
 export default async function Navbar() {
   const user = await currentUser();
 
   return (
     <div className="flex items-center justify-between rounded-full border-2 my-4 max-w-5xl mx-auto px-2 py-2">
-      <div className="bg-[#c9b9ac] rounded-full ">
-        <Logo className="h-14 w-14 p-1" />
-      </div>
+      <Link href={"/"}>
+        <div className="bg-[#c9b9ac] rounded-full ">
+          <Logo className="h-14 w-14 p-1" />
+        </div>
+      </Link>
 
       <div className="scroll-m-20 text-2xl font-semibold">minami</div>
 
