38ff06b283c2f4dd4192c091de061f0ddd3ef998
Piyush <bhangalepiyush@gmail.com>
2023-04-22 18:30:14 +0530

    feat: complete meditation page

--- app/meditate/page.tsx
+++ app/meditate/page.tsx
@@ -1,9 +1,40 @@
+"use client";
+
 import PulseIcon from "@/components/icons/PulseIcon";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function MeditatePage() {
+  const [isMeditating, setMeditating] = useState(false);
+  const [audio, setAudio] = useState(new Audio("/sounds/meditate.ogg"));
+  const [timeInterval, setTimeInterval] = useState<any>(null);
+
+  const toggleMeditating = () => {
+    setMeditating((prev) => !prev);
+
+    if (isMeditating) {
+      audio.pause();
+      audio.currentTime = 0;
+      clearInterval(timeInterval);
+      setTimeInterval(null);
+    } else {
+      audio.play();
+      const interval = setInterval(() => {
+        audio.play();
+      }, 8000);
+      setTimeInterval(interval);
+    }
+  };
+
   return (
-    <div className="h-full flex flex-1 flex-col items-center justify-center">
-      <PulseIcon />
+    <div className="h-full flex flex-1 flex-col items-center justify-center transition-all duration-700">
+      <div className={isMeditating ? "block" : "hidden"}>
+        <PulseIcon />
+      </div>
+
+      <Button className="mt-4" size={"lg"} onClick={toggleMeditating}>
+        {isMeditating ? "Stop meditating" : "Start meditating"}
+      </Button>
     </div>
   );
 }
