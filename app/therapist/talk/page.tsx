47df52725103d747b530d394a593bb60a43284b2
Piyush <bhangalepiyush@gmail.com>
2023-04-23 09:51:09 +0530

    feat: add timer on call page

--- app/therapist/talk/page.tsx
+++ app/therapist/talk/page.tsx
@@ -1,8 +1,20 @@
 "use client";
 
+import dayjs from "dayjs";
 import { PhoneCall } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function TherapistTalk() {
+  const [timeNow, setTimeNow] = useState(dayjs().format("h:mm:ss A"));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeNow(dayjs().format("h:mm:ss A"));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="h-full flex flex-1 flex-col justify-center">
       <div className="h-[70vh] py8 w-full grid grid-cols-12 gap-6">
@@ -16,15 +28,28 @@ export default function TherapistTalk() {
         </div>
 
         <div className="col-span-4">
-          <div className="p-8 bg-[#e8e3d0] rounded-2xl h-full flex flex-col justify-centr gap-4">
-            <div>
-              <div className="uppercase text-center text-xl font-bold">
-                We know,
-                <br />
-                how hard it can be
+          <div className="flex flex-col gap-6">
+            <div className="p-8 bg-[#e8e3d0] rounded-2xl h-full flex flex-col justify-centr gap-4">
+              <div>
+                <div className="uppercase text-center text-xl font-bold">
+                  We know,
+                  <br />
+                  how hard it can be
+                </div>
+                <div className=" text-opacity-40 text-center">
+                  Write everything you want to talk about
+                </div>
               </div>
-              <div className=" text-opacity-40 text-center">
-                Write everything you want to talk about
+            </div>
+
+            <div className="p-8 bg-[#e8e3d0] rounded-2xl h-full flex flex-col justify-centr gap-4">
+              <div className="flex flex-col gap-2">
+                <div className="uppercase text-center text-4xl font-bold">
+                  {timeNow}
+                </div>
+                <div className=" text-opacity-40 text-center">
+                  {dayjs().format("dddd, MMMM D, YYYY")}
+                </div>
               </div>
             </div>
           </div>
