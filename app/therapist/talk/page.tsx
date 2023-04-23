7d5389465fd2804ba32852b1e315c92cf2e15550
Piyush <bhangalepiyush@gmail.com>
2023-04-23 09:47:00 +0530

    feat: changes to color

--- app/therapist/talk/page.tsx
+++ app/therapist/talk/page.tsx
@@ -7,8 +7,8 @@ export default function TherapistTalk() {
     <div className="h-full flex flex-1 flex-col justify-center">
       <div className="h-[70vh] py8 w-full grid grid-cols-12 gap-6">
         <div className=" col-span-8">
-          <div className="h-full bg-[#D9D9D9] rounded-2xl flex flex-col items-center justify-center gap-6">
-            <div className="rounded-full p-6 bg-[#C9B9AC] flex items-center justify-center">
+          <div className="h-full bg-[#2B3242] text-[#e8e3d0] rounded-2xl flex flex-col items-center justify-center gap-6">
+            <div className="rounded-full p-6 bg-[#C9B9AC] text-[#2B3242] flex items-center justify-center">
               <PhoneCall className="h-12 w-12" />
             </div>
             <div className="text-2xl font-bold uppercase">Start a call</div>
